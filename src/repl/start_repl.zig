7b3c394e5711efd5b140cf7407d9044192d41f74
nt2311-vn <nguyentoan231196@gmail.com>
2024-09-27 18:06:41 +0700

    cli not work

--- src/repl/start_repl.zig
+++ src/repl/start_repl.zig
@@ -7,7 +7,7 @@ const ascii = std.ascii;
 const CliCommand = struct {
     name: []const u8,
     description: []const u8,
-    callbackFn: fn () anyerror!void,
+    callbackFn: *const fn () anyerror!void,
 };
 
 const trademarks =
@@ -26,7 +26,7 @@ fn cleanInput(allocator: mem.Allocator, str: []const u8) ![][]const u8 {
         if (word.len > 0) try words.append(try allocator.dupe(u8, word));
     }
 
-    return words;
+    return words.toOwnedSlice();
 }
 
 fn callBackHelp() !void {
@@ -36,13 +36,45 @@ fn callBackHelp() !void {
 fn getCommands(allocator: mem.Allocator) !std.StringHashMap(CliCommand) {
     var commands = std.StringHashMap(CliCommand).init(allocator);
     try commands.put(try allocator.dupe(u8, "help"), .{ .name = "help", .description = "List of available comamnds in costing.", .callbackFn = callBackHelp });
+
+    return commands;
 }
 
 pub fn starRepl() !void {
     debug.print("{s}", .{trademarks});
     debug.print("", .{});
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer gpa.deinit();
+    defer _ = gpa.deinit();
 
-    while (true) {}
+    const allocator = gpa.allocator();
+    const stdin = io.getStdIn().reader();
+    var buffer: [1024]u8 = undefined;
+
+    while (true) {
+        debug.print("costing>", .{});
+        if (try stdin.readUntilDelimiterOrEof(&buffer, '\n')) |input| {
+            const cleaned = try cleanInput(allocator, input);
+            defer allocator.free(cleaned);
+
+            if (cleaned.len == 0) continue;
+            const commandName = cleaned[0];
+            const commands = try getCommands(allocator);
+            defer {
+                var keys = commands.keyIterator();
+                while (keys.next()) |key| {
+                    allocator.free(key.*);
+                }
+            }
+
+            if (commands.get(commandName)) |command| {
+                command.callbackFn() catch |err| {
+                    debug.print("Error: {}\n", .{err});
+                };
+            } else {
+                debug.print("Invalid command\n", .{});
+            }
+        } else {
+            break;
+        }
+    }
 }
