7889602d1d3ffb1c97ec5952436e91e9a1966f26
nt2311-vn <nguyentoan231196@gmail.com>
2024-10-03 16:18:18 +0700

    a slice of constant command in help command

--- src/repl/start_repl.zig
+++ src/repl/start_repl.zig
@@ -15,27 +15,45 @@ pub const CliCommand = struct {
     execFn: *const fn () anyerror!void,
 };
 
+fn helpCommand() !void {
+    const Command = struct { name: []const u8, description: []const u8 };
+
+    const commands: [2]Command = [_]Command{ Command{ .name = "help", .description = "List all available commands" }, Command{ .name = "start", .description = "Start the programming" } };
+
+    for (commands) |command| {
+        debug.print("{s} - {s}\n", .{ command.name, command.description });
+    }
+}
+
 pub fn startRepl() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
     const allocator = gpa.allocator();
     const stdin = std.io.getStdIn().reader();
-    const stdout = std.io.getStdOut().writer();
+    var commands = std.StringHashMap(CliCommand).init(allocator);
+
+    try commands.put("help", .{ .name = "help", .description = "List all the available commands", .execFn = helpCommand });
 
     debug.print("{s}\n", .{trademarks});
 
     while (true) {
         const buf = try allocator.alloc(u8, 120);
         defer allocator.free(buf);
-        try stdout.print("Your input> ", .{});
+        debug.print("Your input> ", .{});
         if (try stdin.readUntilDelimiterOrEof(buf, '\n')) |line| {
             var input = try allocator.dupe(u8, line);
             defer allocator.free(input);
 
             input = @constCast(mem.trimRight(u8, input, "\r\n"));
 
-            debug.print("{s} {d}\n", .{ input, input.len });
+            if (input.len == 0) continue;
+
+            if (commands.get(input)) |command| {
+                try command.execFn();
+            } else {
+                debug.print("Invalid commands\n", .{});
+            }
         }
     }
 }
